7a1695a5f2028285e534d157b8a3bb14010c4a2e
Cryptocode <crytocode99@gmail.com>
2020-12-29 21:50:47 +0700

    Xswap

--- test/XswapStakingRewardsFactory.spec.ts
+++ test/XswapStakingRewardsFactory.spec.ts
@@ -37,7 +37,7 @@ describe('XswapStakingRewardsFactory', () => {
 
   it('deployment gas', async () => {
     const receipt = await provider.getTransactionReceipt(stakingRewardsFactory.deployTransaction.hash)
-    expect(receipt.gasUsed).to.eq('2509106')
+    expect(receipt.gasUsed).to.eq('2080815')
   })
 
   describe('#deploy', () => {
